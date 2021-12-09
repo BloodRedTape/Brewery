9c5ade469552988d1293896c9ee45f2e1bc66e12
BloodRedTape <gefestrale@gmail.com>
2021-12-09 17:15:20 +0200

    Refactoring

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,4 @@
+#include "core/print.hpp"
 #include <core/span.hpp>
 #include <core/os/clock.hpp>
 #include <core/os/window.hpp>
@@ -6,7 +7,50 @@
 #include <graphics/api/swapchain.hpp>
 #include <imgui/backend.hpp>
 #include <sqlite3.h>
+#include <tuple>
 
+class Database{
+private:
+    sqlite3 *m_Handle = nullptr;
+
+    using CallbackType = Function<void(int, char**, char**)>;
+public:
+
+    Database(const char *filepath){
+        sqlite3_open(filepath, &m_Handle);
+    }
+
+    ~Database(){
+        sqlite3_close(m_Handle);
+    }
+
+    template<typename...ArgsType>
+    bool Execute(const char *fmt, ArgsType&&...args){
+        const size_t buffer_size = 4096;
+        struct Buffer{
+            size_t Written = 0;
+            char Data[buffer_size];
+        }buffer;
+
+        buffer.Data[0] = 0;
+
+        auto writer = [](char ch, void *user_data){
+            Buffer *buffer = (Buffer*)user_data;
+            buffer->Data[buffer->Written++] = ch;
+        };
+
+        WriterPrint(writer, &buffer, fmt, Forward<ArgsType>(args)...);
+
+        char *message = nullptr;
+        if(sqlite3_exec(m_Handle, buffer.Data, nullptr, nullptr,&message) != SQLITE_OK){
+            Println("SQLite3: %", message);
+            sqlite3_free(message);
+            return false;
+        }
+
+        return true;
+    }
+};
 
 struct AutoWindow: Window{
     AutoWindow(int width, int height, const char *title){
@@ -19,6 +63,36 @@ struct AutoWindow: Window{
     }
 };
 
+class Dockspace{
+private:
+    const Vector2s m_WindowSize;
+public:
+    Dockspace(Vector2s window_size):
+        m_WindowSize(window_size)
+    {}
+
+    void Draw()const{
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+        ImGui::SetNextWindowSize(ImVec2(m_WindowSize.x, m_WindowSize.y)); 
+        ImGui::SetNextWindowPos({0, 0});
+        ImGuiWindowFlags flags = 0;
+        flags |= ImGuiWindowFlags_NoTitleBar;
+        flags |= ImGuiWindowFlags_NoResize;
+        flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+        flags |= ImGuiWindowFlags_NoMove;
+
+        ImGui::Begin("Brewery", nullptr, flags);
+        {
+            ImGui::DockSpace(ImGui::GetID("Dockspace"));
+        }
+        ImGui::End();
+
+        ImGui::PopStyleVar(2);
+    }
+};
+
 class Application{
 private:
     AutoWindow m_Window{1280, 720, "Brewery"};
@@ -27,11 +101,18 @@ private:
     float FramerateLimit = 60.f;
 
     Semaphore m_Begin, m_End;
+
+    Database m_DB{"brewery.sqlite"};
+
+    RawVar<Dockspace> m_Dockspace;
 public:
     Application(){
         Function<void(const Event&)> handler;
         handler.Bind<Application, &Application::OnEvent>(this);
         m_Window.SetEventsHanlder(handler);
+
+        m_Dockspace.Construct(m_Window.Size());
+
     }
 
     void Run(){
@@ -58,32 +139,17 @@ public:
     }
 
     void OnImGui(){
-        auto size = m_Window.Size();
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-
-        ImGui::SetNextWindowSize(ImVec2(size.x, size.y)); 
-        ImGui::SetNextWindowPos({0, 0});
-        ImGuiWindowFlags flags = 0;
-        flags |= ImGuiWindowFlags_NoTitleBar;
-        flags |= ImGuiWindowFlags_NoResize;
-        flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-        flags |= ImGuiWindowFlags_NoMove;
-
-        ImGui::Begin("Brewery", nullptr, flags);
-        {
-            ImGui::DockSpace(ImGui::GetID("Dockspace"));
-        }
-        ImGui::End();
-
-        ImGui::PopStyleVar(2);
+        m_Dockspace->Draw();
     }
 
     void OnEvent(const Event &e){
         if(e.Type == EventType::WindowClose)
             m_Window.Close();
-        if(e.Type == EventType::WindowResized)
+        if(e.Type == EventType::WindowResized){
             m_Swapchain.Recreate();
+            m_Dockspace.Destruct();
+            m_Dockspace.Construct(Vector2s(e.WindowResized.x, e.WindowResized.y));
+        }
 
         m_Backend.HandleEvent(e);
     }
